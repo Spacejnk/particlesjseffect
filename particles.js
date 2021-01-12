7e319258f7234058992c902f1eb7f1ad8198f6d4
Timits <will4f21@yahoo.com>
2021-01-12 05:22:12 -0800

    update particle with collision detection and draw() method

--- particles.js
+++ particles.js
@@ -29,6 +29,50 @@ class Particle {
         this.size;
         this.color;
     }
+    // draw method for each particle 
+    draw() {
+        ctx.beginPath();
+        ctx.arc( this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#fd86d1';
+        ctx.fill();
+    }
+
+    // check particle position, check mouse position, move the particle, draw the particle
+    update() {
+        // check if particle is still within canvas
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        // Circle Collision Mozilla
+        // check collision detection - mouse position / particle position
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx*dx + dy*dy);
+        if (distance < mouse.radius + this.size){
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y > this.size * 10) {
+                this.y -=  10
+            }
+            // move the particle
+            this.x += this.directionX;
+            this.y += this.directionY;
+            // draw particle
+            this.draw()
+        }
+    }
+
 }
 
 
