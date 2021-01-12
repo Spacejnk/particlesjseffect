f31285d32a7e0fc80e354dcb70bfced577d6ba2d
Timits <will4f21@yahoo.com>
2021-01-12 04:43:58 -0800

    adjust canvas width/height add event listener

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Particles Vanilla JavaScript 
+* HTML Canvas
+* Vanilla JavaScript
+* CSS
+* [Radial-Gradient](https://uigradients.com/#Alihossein)
+<!-- * [GIF with Cloud Convert](https://cloudconvert.com/) -->
+
+<!-- ![caption](image/particles.gif) -->
+
---
+++ particles.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.width = window.innerHeight;
+
+let particlesArray;
+
+// get mouse positon 
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+
+window.addEventListener('mousemove', 
+        function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+)
+
